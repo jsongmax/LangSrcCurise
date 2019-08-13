b585d6e7c9f0e0bac7ada6c301d5cc57dd014a4c
LangziFun <982722261@qq.com>
2019-08-14 00:44:56 +0800

    获取信息平均值

--- LangSrcCuriseOnLinux/core/Cor.py
+++ LangSrcCuriseOnLinux/core/Cor.py
@@ -11,21 +11,30 @@ def network():
     p = psutil
     before_recv = p.net_io_counters().bytes_recv
     before_send = p.net_io_counters().bytes_sent
-
-    time.sleep(3600)
+    cpu_1 = psutil.virtual_memory().percent
+    men_1 = psutil.cpu_percent(True)
+    time.sleep(1200)
+    cpu_2 = psutil.virtual_memory().percent
+    men_2 = psutil.cpu_percent(True)
+    time.sleep(1200)
+    cpu_3 = psutil.virtual_memory().percent
+    men_3 = psutil.cpu_percent(True)
+    time.sleep(1200)
     # 每个小时
+    cpu = ((cpu_1+cpu_2+cpu_3)/3)
+    men = ((men_1+men_2+men_3)/3)
     now_recv = p.net_io_counters().bytes_recv
     now_send = p.net_io_counters().bytes_sent
 
     delta_send = (now_send - before_send) / 1024000
     delta_recv = (now_recv - before_recv) / 1024000
-    return (int(delta_send),int(delta_recv))
+    return (str(cpu),str(men),int(delta_send),int(delta_recv))
 
 def Cor():
     # 返回一个小时内，CPU/内存 使用率% 和使用的宽带 上传/下载量 M
     new = network()
     # 返回 ('25.7%', '2.3%', '0Mb', '0Mb')
-    return (str(psutil.virtual_memory().percent)+'%',str(psutil.cpu_percent(True))+'%',str(new[0])+'Mb',str(new[1])+'Mb')
+    return (str(new[0])+'%',str(new[1])+'%',str(new[2])+'Mb',str(new[3])+'Mb')
 
 if __name__ == '__main__':
     print('内存使用率:{}%'.format(psutil.virtual_memory().percent))
